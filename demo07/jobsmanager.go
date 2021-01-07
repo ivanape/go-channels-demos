b3d109343525f075688989223d36f78b2b940cd8
Iván Álvarez <ialvarezpereira@gmail.com>
2021-01-07 20:40:51 +0100

    Review JobManager API

--- demo07/jobsmanager.go
+++ demo07/jobsmanager.go
@@ -127,12 +127,3 @@ func (j *JobsManager) registerWorker() {
 		}
 	}
 }
-
-func contains(s []string, e string) bool {
-	for _, a := range s {
-		if a == e {
-			return true
-		}
-	}
-	return false
-}
